82180d1c879a1437302ef875ad718234487706ca
oobrucero <bpreier2@students.wcpss.net>
2021-06-19 20:15:29 -0400

    :P

--- clock.js
+++ clock.js
@@ -17,9 +17,9 @@ function startTime() {
   
   document.getElementById('clk').innerHTML =
   "&#127482;&#127480; " + ph + ":" + m + ":" + s +
-  "🇪🇺 " + eh + ":" + m + ":" + s ;
+  "/&#127482;&#127480;" + eh + ":" + m + ":" + s ;
   document.getElementById('clk2').innerHTML = 
-  "&#127468;&#127463; " +lh + ":" + m + ":" + s +
+  "🇪🇺 " +lh + ":" + m + ":" + s +
   "/&#127471;&#127477; " + jh + ":" + m + ":" + s ;
   var t = setTimeout(startTime, 500);
 }
